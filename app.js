ef6c30ae06b24de912da558ea513fb1f0250ba38
Naveenkumar P <105690144+naveenkumarp205@users.noreply.github.com>
2023-10-19 21:42:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -54,13 +54,13 @@ window.onload(
 
 function playerWin(){
     if(Random1Number1==Random1Number2){
-        whoWins.innerHTML="Tide"
+        whoWins.innerHTML="Draw"
     }
     else if(Random1Number1 > Random1Number2){
-        whoWins.innerHTML="Player 1 Wins";
+        whoWins.innerHTML="🚩 Player 1 Wins";
     }
     else if(Random1Number1 < Random1Number2){
-        whoWins.innerHTML="Player 2 Wins";
+        whoWins.innerHTML="Player 2 Wins 🚩";
     }
     else{
         whoWins.innerHTML="Refresh Page";
